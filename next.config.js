162afe21ab974b6caaa3f4aa3f953549fbf28f1f
Ravi S Singh <factedu@gmail.com>
2023-07-01 17:27:21 +0530

    get movies

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: [
+            'localhost',
+            'res.cloudinary.com',
+            'images.unsplash.com',
+            'cdn.pixabay.com',
+            'cdn.dribbble.com',
+            'cdn.fakercloud.com',
+            'cdn-icons-png.flaticon.com',
+            'image.tmdb.org',
+            'www.themoviedb.org',
+        ],
+    }
+}
 
 module.exports = nextConfig
