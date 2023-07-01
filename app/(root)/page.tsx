106a4c4387df59502c6ea1976348354df6cd95fc
Ravi S Singh <factedu@gmail.com>
2023-07-01 16:43:15 +0530

    theme setedup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,10 +3,10 @@ import Image from 'next/image'
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="flex h-[400px] flex-col items-center justify-between p-24">
             <div className="flex flex-col items-center justify-center font-extrabold text-2xl ">Hello App</div>
-            <p className="text-center">Boiler plate by <span className='font-semibold'>Ravi S Singh</span></p>
+            <p className="text-center">NextJS 13 Boilerplate by <span className='font-semibold'>Ravi S Singh</span></p>
             <Button>Click Me</Button>
-        </main>
+        </div>
     )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import Link from 'next/link'
-import prismadb from '@/lib/prismadb'
+import { ThemeProvider } from '@/components/theme-provider'
+import { ModeToggle } from '@/components/mode-toggle'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,19 +21,29 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>
-          <header className="flex items-center justify-between p-4">
-            <h1 className="text-2xl font-bold"><Link href={'/'}>MovieT</Link></h1>
-            <SignedIn>
-              <Link href={'/watch-next'}>Watch Next</Link>
-              <UserButton afterSignOutUrl='/' />
-            </SignedIn>
-            <SignedOut>
-              <SignInButton />
-            </SignedOut>
-          </header>
-          {children}
-        </body>
+        <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+          <body className={inter.className}>
+            <div className='min-h-screen'>
+              <header className="flex items-center gap-3 justify-between px-4 py-2 border-b shadow-sm">
+                <h1 className="text-2xl font-bold"><Link href={'/'}>MovieT</Link></h1>
+                <div className='flex-1 items-center content-center '>
+                  <Link href={'/movies'}>Movies</Link>
+                </div>
+                <div className='flex justify-between gap-4 items-center'>
+                  <SignedIn>
+                    <UserButton afterSignOutUrl='/' />
+                  </SignedIn>
+                  <SignedOut>
+                    <SignInButton />
+                  </SignedOut>
+                  <ModeToggle />
+                </div>
+              </header>
+              {children}
+            </div>
+
+          </body>
+        </ThemeProvider>
       </html>
     </ClerkProvider>
   )
